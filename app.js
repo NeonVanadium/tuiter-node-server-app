5950ed7731b20f15130a76afc91750ffe39213fc
Jack Whitten <jackwhitten@outlook.com>
2023-04-01 21:44:01 -0400

    mongo

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import cors from 'cors'
-import HelloController
-  from "./controllers/hello-controller.js"
-import UserController
-  from "./controllers/users/users-controller.js"
-import TuitsController
-  from "./controllers/tuits/tuits-controller.js";
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+
 const app = express()
 app.use(express.json());
 app.use(cors())
