42b79a402878e07c7ca49be8086843c11f9e34ae
Jack Whitten <jackwhitten@outlook.com>
2023-04-01 22:24:32 -0400

    constring

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING = "mongodb+srv://jackwhitten:supersecretpassword@cluster0.vf1vxwf.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(express.json());
